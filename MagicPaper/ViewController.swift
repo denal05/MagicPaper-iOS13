01bc69437fb858672d16c7fa3fa4f018b247f386
Denis Aleksandrov <denis.aleksandrov@codewell.mk>
2020-12-31 19:06:54 +0100

    Finished module: Section 30. ARKit 2 & Live Video

--- MagicPaper/ViewController.swift
+++ MagicPaper/ViewController.swift
@@ -3,76 +3,86 @@
 //  MagicPaper
 //
 //  Created by Denis Aleksandrov on 12/30/20.
-//
+//  https://svetotnabibi.mk/svetot-na-bibi-od-a-do-sh
+//  https://deca.mk/category/bibiverzum/
+//  http://bibiland.mk
 
 import UIKit
 import SceneKit
 import ARKit
 import youtube_ios_player_helper
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController, ARSCNViewDelegate, YTPlayerViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
-    @IBOutlet weak var playerView: YTPlayerView!
+    @IBOutlet weak var ytPlayerView: YTPlayerView!
+    
+    static let vAzbukataNaBibiId  = "VyyyNQn0-jw"
+    static let vAzbukataNaBibiUrl = "https://www.youtube.com/watch?v=" + vAzbukataNaBibiId
+    static let vAzbukataNaBibiMp4 = "azbukata-na-bibi.mp4"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Set the view's delegate
         sceneView.delegate = self
+        ytPlayerView.delegate = self
         
-        // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
-        
-        playerView.load(withVideoId: "YE7VzlLtp-4", playerVars: ["playsinline": "1"])
+        //ytPlayerView.load(withVideoId: ViewController.vAzbukataNaBibiId, playerVars: ["playsinline": "1"])
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
+        let configuration = ARImageTrackingConfiguration()
+        
+        if let trackedImages = ARReferenceImage.referenceImages(inGroupNamed: "ImagesOnPaper", bundle: Bundle.main) {
+            configuration.trackingImages = trackedImages
+            configuration.maximumNumberOfTrackedImages = trackedImages.count
+        }
 
-        // Run the view's session
         sceneView.session.run(configuration)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
-        // Pause the view's session
+
         sceneView.session.pause()
     }
 
     // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
+    // The anchor is the tracked image that was found
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
         let node = SCNNode()
-     
+        
+        if let imageAnchor = anchor as? ARImageAnchor {
+            let videoNode = SKVideoNode(fileNamed: ViewController.vAzbukataNaBibiMp4)
+            videoNode.play()
+            let videoScene = SKScene(size: CGSize(width: 480, height: 360))
+            videoNode.position = CGPoint(
+                x: videoScene.size.width / 2,
+                y: videoScene.size.height / 2
+            )
+            videoNode.yScale = -1.0
+            videoScene.addChild(videoNode)
+            
+            // https://hackernoon.com/playing-videos-in-augmented-reality-using-arkit-7df3db3795b7
+            //let webView = UIWebView(frame: CGRect(x: 0, y: 0, width: 640, height: 480))
+            //let request = URLRequest(url: URL(string: "http://www.amazon.com")!)
+            //webView.loadRequest(request)
+            
+            let plane = SCNPlane(
+                width:  imageAnchor.referenceImage.physicalSize.width,
+                height: imageAnchor.referenceImage.physicalSize.height
+            )
+            plane.firstMaterial?.diffuse.contents = videoScene //webView //UIColor(white: 1.0, alpha: 0.5)
+            let planeNode = SCNNode(geometry: plane)
+            planeNode.eulerAngles.x = -.pi / 2
+            node.addChildNode(planeNode)
+        }
+        
         return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
     }
 }
